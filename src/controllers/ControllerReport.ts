edd79050032dd2940652622fe01d858e19415016
Brian Pina <brian.pina2001@gmail.com>
2021-03-24 23:00:40 -0300

    Add html template and pdf rendering

--- src/controllers/ControllerReport.ts
+++ src/controllers/ControllerReport.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import handlebars from 'handlebars';
+import { create } from 'html-pdf';
+import { reportTemplate } from "../templates/report";
 
 export class ControllerReport {
 
@@ -19,28 +21,25 @@ export class ControllerReport {
             });
             if(missingFields.length > 0) resp.status(400).json({"msg": `Missing fields [${missingFields}]`})
 
-            const template = `
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>{{title}}</title>
-                </head>
-                <body>
-                    <h1>{{title}}</h1>
-                    <h3>Denúncia criada em {{date}}</h3>
-                    <h3>{{votes}} pessoas reportaram esse problema</h3>
-                    <p>{{description}}</p>
-                </body>
-                </html>
-            `;
-
             const data = req.body;
-            const htmlDelegate = handlebars.compile(template);
+            const htmlDelegate = handlebars.compile(reportTemplate);
             const html = htmlDelegate(data);
-            resp.send(html);
+            
+            const options = {};
+
+            create(html, options).toStream((err, pdfStream) => {
+                if(err) {
+                    return resp.status(400).json({"message": err});
+                } else {
+                    resp.statusCode = 201;
+                    pdfStream.on('end', () => {
+                        return resp.end();
+                    });
+
+                    pdfStream.pipe(resp);
+                }
+            });
+        
         } catch(err) {
             console.error(err);
             resp.status(400).json({'msg': err});
