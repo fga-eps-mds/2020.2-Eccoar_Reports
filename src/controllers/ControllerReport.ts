beaf110b94ccd405d3066c0d1084fffc16512d9d
Brian Pina <brian.pina2001@gmail.com>
2021-03-27 09:35:02 -0300

    Change pdf generation

--- src/controllers/ControllerReport.ts
+++ src/controllers/ControllerReport.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
 import handlebars from 'handlebars';
-import { create } from 'html-pdf';
-import { reportTemplate } from "../templates/report";
+import { create as createPDF, FileInfo } from 'html-pdf';
+import {readFileSync, createWriteStream} from 'graceful-fs';
 
 export class ControllerReport {
-
     async pong (req: Request, resp: Response): Promise<void> {
         const pingPong = {
             ping: "pong"
@@ -15,19 +14,23 @@ export class ControllerReport {
     async createReport(req: Request, resp: Response): Promise<void>{
         try {
             let missingFields:string[] = [];
-            const fields = ['title', 'date', 'votes', 'description'];
+            const categories = {'Hole': 'Buraco', 'Energy': 'Eletricidade', 'Water': 'Água'};
+            const fields = ['title', 'date', 'votes', 'description', 'category'];
             fields.forEach(field => {
                 if(!(field in req.body)) missingFields.push(field);
             });
             if(missingFields.length > 0) resp.status(400).json({"msg": `Missing fields [${missingFields}]`})
 
-            const data = req.body;
+            const reportTemplate = readFileSync('src/templates/report.handlebars', 'utf-8');
+
+            let data = req.body;
+            data['category'] = categories[data['category']];
             const htmlDelegate = handlebars.compile(reportTemplate);
             const html = htmlDelegate(data);
             
             const options = {};
 
-            create(html, options).toStream((err, pdfStream) => {
+            /* create(html, options).toStream((err, pdfStream) => {
                 if(err) {
                     return resp.status(400).json({"message": err});
                 } else {
@@ -38,8 +41,14 @@ export class ControllerReport {
 
                     pdfStream.pipe(resp);
                 }
+            }); */
+            createPDF(html, options).toStream((err, stream) => {
+                if(err) return resp.status(400).json({"message": err});
+
+                stream.pipe(createWriteStream('./file.pdf'));
+
+                return resp.status(201).json({"message": "File created at ./report.pdf"});
             });
-        
         } catch(err) {
             console.error(err);
             resp.status(400).json({'msg': err});
