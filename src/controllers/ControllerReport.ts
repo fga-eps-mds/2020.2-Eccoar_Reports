00ddbabdeae19a9eba1d601442d92a21075c19a9
Brian Pina <brian.pina2001@gmail.com>
2021-03-27 14:00:25 -0300

    Render multiple reports

--- src/controllers/ControllerReport.ts
+++ src/controllers/ControllerReport.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import handlebars from 'handlebars';
-import { create as createPDF, FileInfo } from 'html-pdf';
+import { create as createPDF, CreateOptions, FileInfo } from 'html-pdf';
 import {readFileSync, createWriteStream} from 'graceful-fs';
 
 export class ControllerReport {
@@ -13,41 +13,26 @@ export class ControllerReport {
 
     async createReport(req: Request, resp: Response): Promise<void>{
         try {
-            let missingFields:string[] = [];
-            const categories = {'Hole': 'Buraco', 'Energy': 'Eletricidade', 'Water': 'Água'};
-            const fields = ['title', 'date', 'votes', 'description', 'category'];
-            fields.forEach(field => {
-                if(!(field in req.body)) missingFields.push(field);
-            });
-            if(missingFields.length > 0) resp.status(400).json({"msg": `Missing fields [${missingFields}]`})
+
 
             const reportTemplate = readFileSync('src/templates/report.handlebars', 'utf-8');
 
             let data = req.body;
-            data['category'] = categories[data['category']];
             const htmlDelegate = handlebars.compile(reportTemplate);
             const html = htmlDelegate(data);
             
-            const options = {};
+            const options: CreateOptions = {};
 
-            /* create(html, options).toStream((err, pdfStream) => {
-                if(err) {
-                    return resp.status(400).json({"message": err});
-                } else {
-                    resp.statusCode = 201;
-                    pdfStream.on('end', () => {
-                        return resp.end();
-                    });
-
-                    pdfStream.pipe(resp);
-                }
-            }); */
             createPDF(html, options).toStream((err, stream) => {
                 if(err) return resp.status(400).json({"message": err});
 
                 stream.pipe(createWriteStream('./file.pdf'));
 
-                return resp.status(201).json({"message": "File created at ./report.pdf"});
+                resp.statusCode = 201;
+                stream.on('end', () => {
+                    resp.end();
+                });
+                stream.pipe(resp);
             });
         } catch(err) {
             console.error(err);
