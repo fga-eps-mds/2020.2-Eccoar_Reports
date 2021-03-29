387bb26f31a340a139539b1308492add5cd52cf8
Brian Pina <brian.pina2001@gmail.com>
2021-03-29 18:57:29 -0300

    Update image to base64 in template

--- src/controllers/ControllerReport.ts
+++ src/controllers/ControllerReport.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
-import {compile as compileHTML} from 'handlebars';
-import { create as createPDF, CreateOptions, FileInfo } from 'html-pdf';
+import {compile as compileHTML, registerHelper as hbRegisterHelper} from 'handlebars';
+import { create as createPDF, CreateOptions } from 'html-pdf';
 import {readFileSync, createWriteStream} from 'graceful-fs';
+import * as imageToBase64 from 'image-to-base64';
 
 export class ControllerReport {
     async pong (req: Request, resp: Response): Promise<void> {
@@ -14,29 +15,36 @@ export class ControllerReport {
     async createReport(req: Request, resp: Response): Promise<void>{
         try {
 
+            hbRegisterHelper('exists', (value) => {
+                return value !== null;
+            });
+
             const img = readFileSync('src/assets/logo.png').toString('base64');
-
             const reportTemplate = readFileSync('src/templates/report.handlebars', 'utf-8');
-
             let data = {...req.body, img: `data:image/png;base64,${img}`};
             const htmlDelegate = compileHTML(reportTemplate);
+            
+            let height = 0;
+            for(let i = 0; i < data.complaints.length; i++) {
+                if(data.complaints[i].image !== null) {
+                    data.complaints[i].image = await imageToBase64(data.complaints[i].image);
+                    height+=500;
+                }
+                else height+=290;
+            }
             const html = htmlDelegate(data);
-
-
             const options: CreateOptions = {
-                height: `${(data.complaints.length * 500) + 250}px`,
+                height: `${height + 250}px`,
                 width: '900px'
             };
-
             createPDF(html, options).toStream((err, stream) => {
                 if(err) return resp.status(400).json({"message": err});
-
                 stream.pipe(createWriteStream(`src/${req.body.category}.pdf`));
             });
 
             resp.status(201).json({"msg": `Report created at src/${req.body.category}.pdf`});
         } catch(err) {
-            resp.status(400).json({'msg': err});
+            resp.status(400).json({'msg': err.message});
         }
     }
 }
