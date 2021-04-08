d586466144912370f6bd2a158fcfd279b8cde494
Arthur-Matos <git.arthurmatos@gmail.com>
2021-04-08 19:23:30 -0300

    Configure Morgan middleware

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import * as express from 'express';
 import routers from './routes';
 import * as cors from 'cors';
+import * as morgan from "morgan";
 
 const app = express();
 const PORT = process.env.APP_PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan('combined'));
 app.use(routers);
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
+    console.log(`Listening on port ${PORT}`);
 });
