e8657d104814e9af60be26305c9640461436b02f
phe0 <pheofo@gmail.com>
2021-04-10 15:22:48 -0300

    fix test ignorepath to jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	testPathIgnorePatterns: ['build'],
+	testPathIgnorePatterns: ['<rootDir>/build'],
 	moduleNameMapper: {
 		'@controllers/(.*)': '<rootDir>/src/controllers/$1',
 		'@utils/(.*)': '<rootDir>/src/utils/$1',
